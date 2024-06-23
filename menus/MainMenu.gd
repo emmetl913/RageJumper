52ddf187506168943fd6bdb49bb416f51514765c
emmetl913 <elars1002@gmail.com>
2024-06-23 14:58:28 -0400

    main menu, pause screen

--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -1,8 +1,13 @@
 extends Control
 
 
-
+func _ready():
+	get_tree().paused = false
 
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://worlds/main.tscn")
