9327ed7cff52bf6ac026b58ddddef238592b65bd
calebj205 <calebj205@gmail.com>
2024-06-23 16:55:24 -0400

    Basic level created, camera movement implemented

--- worlds/main.gd
+++ worlds/main.gd
@@ -3,11 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#Camera controls
+	$Camera2D.position.y = 128 + (floor($Player.position.y / 256) * 256)
+	
 	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = not get_tree().paused
 		if get_tree().paused:
