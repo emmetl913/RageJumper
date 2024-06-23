4f693e18bc3c7ebee089d4b557b389791d4c8a84
bwoodward523 <bwoodward@ycp.edu>
2024-06-23 14:34:51 -0400

    Dude added!

---
+++ player/PlayerMovement.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var jump_speed = -600
+@export var gravity = 1000
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	
+	velocity.x = Input.get_axis("move_left", "move_right") * speed
+	
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
